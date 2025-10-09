43a2bb4c2787f1e4fbadaa80676d2ed5b8cb0d3e
Volodymyr Kutovyi <vladimir.kutovoy@gentlecode.com>
2025-10-09 20:26:53 +0300

    User model: made street and postal/zip code optional, added "Save %" field.

--- hockey_baseball_app_backend/users/models.py
+++ hockey_baseball_app_backend/users/models.py
@@ -13,8 +13,9 @@ class CustomUser(AbstractUser):
     country = models.CharField(_("country"), max_length=100)
     region = models.CharField(_("province/state"), max_length=100)
     city = models.CharField(_("city"), max_length=100)
-    street = models.CharField(_("street"), max_length=200)
-    postal_code = models.CharField(_("postal/zip code"), max_length=50)
+    street = models.CharField(_("street"), max_length=200, blank=True)
+    postal_code = models.CharField(_("postal/zip code"), max_length=50, blank=True)
+    save_percents = models.IntegerField(_("save %"), null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
